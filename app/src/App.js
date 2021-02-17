79c63163cad6db9c159b48772243c9d3f8ba41c3
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-02-17 16:21:55 -0600

    Currently having an error that causes a failure to compile. Redux can't resolve itself.

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react'
+import Display from "./components/display"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> You Prolly Oughta Replace This Placeholder Huh?  </h1>
+     <h2> Remembewr, you can actually place this kinda stuff inside the componenet, or just style it here i guess  </h2>
+     <Display />
     </div>
   );
 }
---
+++ app/src/components/display.js
@@ -0,0 +1,14 @@
+// import styled components or something
+import React, { useEffect } from 'react'
+import { connect } from "react-redux"
+
+function display() {
+
+    return (
+      <div className="container">
+       
+      </div>
+    );
+  }
+  
+  export default connect()(display);
